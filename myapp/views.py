dd811c5322cd99dc6ed7f5e066cfe88a8a758130
surajsmane11-ship-i <surajsmane11@gmail.com>
2026-04-26 13:29:18 +0530

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("🚀 Hello, Jenkins CI/CD is working!")
+    return HttpResponse("🚀 Hello, Jenkins CI/CD is working! POLL SCM AUTO BUILD IN 2 MIN")
